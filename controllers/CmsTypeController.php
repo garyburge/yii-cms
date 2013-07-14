815a33650851efe13bf0c6707f3d7b64bb91d022
Gary Burge <garyburge@garyburge.com>
2013-07-14 03:45:34 -0400

    Add check to delete type action to determine if type has been assigned to an item; other changes

--- controllers/CmsTypeController.php
+++ controllers/CmsTypeController.php
@@ -2,176 +2,197 @@
 
 class CmsTypeController extends Controller
 {
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-		);
-	}
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',
-				  'actions'=>array('admin', 'view', 'create', 'update'),
-				  'roles'=>array('editor', 'publisher', 'administrator'),
-			),
-			array('allow',
-				  'actions'=>array('publish', 'delete'),
-				  'roles'=>array('publisher', 'administrator'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
 
-	/**
-	 * Lists all models.
-	 */
-	public function actionIndex()
-	{
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                'actions'=>array('admin', 'view', 'create', 'update'),
+                'roles'=>array('editor', 'publisher', 'administrator'),
+            ),
+            array('allow',
+                'actions'=>array('delete'),
+                'roles'=>array('publisher', 'administrator'),
+            ),
+            array('deny', // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function actionIndex()
+    {
         // create data provider
-		$dataProvider = new CActiveDataProvider('CmsType');
+        $dataProvider = new CActiveDataProvider('CmsType');
 
         // render list of types
-		$this->render('index', array(
-			'dataProvider'=>$dataProvider,
-		));
-	}
+        $this->render('index', array(
+            'dataProvider'=>$dataProvider,
+        ));
+    }
 
-	/**
-	 * Manages all models.
-	 */
-	public function actionAdmin()
-	{
+    /**
+     * Manages all models.
+     */
+    public function actionAdmin()
+    {
         // create model
-		$model = new CmsType('search');
+        $model = new CmsType('search');
 
         // clear any defaults
-		$model->unsetAttributes();
+        $model->unsetAttributes();
 
         // if paging information available
-		if(isset($_GET['CmsType'])) {
+        if (isset($_GET['CmsType'])) {
             // copy it to model
-			$model->attributes=$_GET['CmsType'];
+            $model->attributes = $_GET['CmsType'];
         }
 
         // render table of types
-		$this->render('admin', array(
-			'model'=>$model,
-		));
-	}
+        $this->render('admin', array(
+            'model'=>$model,
+        ));
+    }
 
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionView($id)
-	{
-		$this->render('view',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
+    /**
+     * Displays a particular model.
+     * @param integer $id the ID of the model to be displayed
+     */
+    public function actionView($id)
+    {
+        $this->render('view', array(
+            'model'=>$this->loadModel($id),
+        ));
+    }
 
-	/**
-	 * Creates a new model.
-	 * If creation is successful, the browser will be redirected to the 'view' page.
-	 */
-	public function actionCreate()
-	{
-		$model=new CmsType;
+    /**
+     * Creates a new model.
+     */
+    public function actionCreate()
+    {
+        // create model
+        $model = new CmsType;
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['CmsType']))
-		{
-			$model->attributes=$_POST['CmsType'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        // if form data available
+        if (isset($_POST['CmsType'])) {
+            // copy form data to model
+            $model->attributes = $_POST['CmsType'];
+            // validate and save
+            if ($model->save()) {
+                // return to admin grid
+                $this->redirect(array('admin'));
+            }
+        }
 
-		$this->render('create',array(
-			'model'=>$model,
-		));
-	}
+        // display form
+        $this->render('create', array(
+            'model'=>$model,
+        ));
+    }
 
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
+    /**
+     * Updates a particular model.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function actionUpdate($id)
+    {
+        // create model for specified row
+        $model = $this->loadModel($id);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
+        // Uncomment the following line if AJAX validation is needed
+        // $this->performAjaxValidation($model);
 
-		if(isset($_POST['CmsType']))
-		{
-			$model->attributes=$_POST['CmsType'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
+        // if form data available
+        if (isset($_POST['CmsType'])) {
+            // copy it to model
+            $model->attributes = $_POST['CmsType'];
+            // validate and save
+            if ($model->save()) {
+                // return to admin grid
+                $this->redirect(array('admin'));
+            }
+        }
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+        // display form
+        $this->render('update', array(
+            'model'=>$model,
+        ));
+    }
 
-	/**
-	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'admin' page.
-	 * @param integer $id the ID of the model to be deleted
-	 */
-	public function actionDelete($id)
-	{
-		if(Yii::app()->request->isPostRequest)
-		{
-			// we only allow deletion via POST request
-			$this->loadModel($id)->delete();
+    /**
+     * Deletes a particular model.
+     * If deletion is successful, the browser will be redirected to the 'admin' page.
+     * @param integer $id the ID of the model to be deleted
+     */
+    public function actionDelete($id)
+    {
+        // we only allow deletion via POST request
+        if (Yii::app()->request->isPostRequest) {
+            // create item to type model
+            $item = new ItemType;
 
-			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
-	}
+            // any item assigned to this type?
+            if ($item->exists('type_id = :type_id', array(':type_id'=>$id))) {
+                throw new CHttpException(400, 'Unable to delete the requested type. That type has been assigned to one or more content items.');
+            }
 
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer the ID of the model to be loaded
-	 */
-	public function loadModel($id)
-	{
-		$model=CmsType::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}
+            // delete the row
+            $this->loadModel($id)->delete();
 
-	/**
-	 * Performs the AJAX validation.
-	 * @param CModel the model to be validated
-	 */
-	protected function performAjaxValidation($model)
-	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='content-type-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	}
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_GET['ajax'])) {
+                $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+            }
+
+        } else {
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request.');
+        }
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer the ID of the model to be loaded
+     */
+    public function loadModel($id)
+    {
+        $model = CmsType::model()->findByPk($id);
+        if ($model === null)
+            throw new CHttpException(404, 'The requested page does not exist.');
+        return $model;
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'content-type-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
 }
