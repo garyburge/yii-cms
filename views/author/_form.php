d2c2974a425fbc8fcb16c5399ac12a4524450f8c
Gary Burge <garyburge@garyburge.com>
2013-07-30 14:56:10 -0400

    Phone missing from _form

--- views/author/_form.php
+++ views/author/_form.php
@@ -10,11 +10,12 @@
 	<?php echo $form->textFieldRow($model,'first_name',array('class'=>'span5','maxlength'=>64)); ?>
 	<?php echo $form->textFieldRow($model,'middle_name',array('class'=>'span5','maxlength'=>64)); ?>
 	<?php echo $form->textFieldRow($model,'last_name',array('class'=>'span5','maxlength'=>64)); ?>
-	<?php echo $form->textFieldRow($model,'url',array('class'=>'span5','maxlength'=>255)); ?>
+	<?php echo $form->textFieldRow($model,'phone',array('class'=>'span4','maxlength'=>24)); ?>
 	<?php echo $form->textFieldRow($model,'email',array('class'=>'span5','maxlength'=>128)); ?>
+	<?php echo $form->textFieldRow($model,'url',array('class'=>'span5','maxlength'=>255)); ?>
 	<?php echo $form->textAreaRow($model,'short_bio',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 	<?php echo $form->textAreaRow($model,'bio',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
-    
+
 	<?php echo $form->textFieldRow($model,'media_id',array('class'=>'span5','maxlength'=>128)); ?>
 
 	<div class="form-actions">
