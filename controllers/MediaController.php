3db8dee96f1459b7f2d3566efaf5c6e0a943cece
Gary Burge <garyburge@garyburge.com>
2013-07-31 06:44:36 -0400

    Add EDropzone widget for dropzone.js

---
+++ controllers/MediaController.php
@@ -0,0 +1,66 @@
+<?php
+
+class MediaController extends Controller
+{
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+            array('allow',
+                  'actions'=>array('upload'),
+                  'roles'=>$this->module->authRolesMedia,
+            ),
+			array('deny',  // deny all users
+				  'users'=>array('*'),
+			),
+		);
+	}
+
+	/**
+	 * upload a file
+	 */
+	public function actionUpload()
+	{
+	}
+
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer the ID of the model to be loaded
+	 */
+	public function loadModel($id)
+	{
+		$model = Media::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param CModel the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='media-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
--- views/author/_form.php
+++ views/author/_form.php
@@ -2,7 +2,9 @@
 	'id'=>'author-form',
     'focus'=>array($model, 'first_name'),
 	'enableAjaxValidation'=>false,
-)); ?>
+));
+echo CHtml::activeHiddenField($model,'media_id');
+?>
 
 	<p class="help-block muted">Fields with <span class="required">*</span> are required.</p>
 	<?php echo $form->errorSummary($model); ?>
@@ -16,7 +18,32 @@
 	<?php echo $form->textAreaRow($model,'short_bio',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 	<?php echo $form->textAreaRow($model,'bio',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
-	<?php echo $form->textFieldRow($model,'media_id',array('class'=>'span5','maxlength'=>128)); ?>
+    <div class="row">
+        <div class="span6">
+            <ul class="thumbnails">
+                <li class="span4">
+                    <div class="thumbnail">
+                    <?php if ($model->id && $model->media): ?>
+                        <img src="<?php echo $model->media->file; ?>" alt="image of author" title="<?php echo $model->media->file.' ('.$model->media->width.'x'.$model->media->height.')'; ?>">
+                        <div><strong><?php echo $model->media->title; ?></strong> - <?php echo $model->media->caption; ?></div>
+                    <?php else: ?>
+                        <div style="width:150px; height:150px; border:1px solid #eee;"></div>
+                    <?php endif; ?>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="span6">
+            <?php $this->widget('dropzone.EDropzone', array(
+                'model' => $model,
+                'attribute' => 'media_id',
+                'url' => $this->createUrl('media/upload'),
+                'mimeTypes' => array('image/jpeg', 'image/png'),
+                'onSuccess' => 'someJsFunction();',
+                'options' => array(),
+            )); ?>
+        </div>
+    </div><!-- .row -->
 
 	<div class="form-actions">
 		<?php $this->widget('bootstrap.widgets.TbButton', array(
