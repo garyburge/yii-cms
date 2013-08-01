5effaa086b1ec1c1c2ed6e210d73ecb075dadc31
Gary Burge <garyburge@garyburge.com>
2013-08-01 16:39:27 -0400

    Still more playing with CUploadedFile

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -7,19 +7,19 @@
 class UploadForm extends CFormModel
 {
     public $file;
-    public $name;
-    public $type;
-    public $tmp_name;
-    public $error;
-    public $size;
-    public $upload;
-    public $media_id;
+//    public $name;
+//    public $type;
+//    public $tmp_name;
+//    public $error;
+//    public $size;
+//    public $upload;
+//    public $media_id;
 
     public function rules()
     {
         return array(
-            array('name', 'file', 'types'=>'jpg, gif, png'),
-            array('file, name, type, tmp_name, error, size. upload, media_id', 'safe'=>true, 'on'=>'upload')
+            array('file', 'file', 'allowEmpty'=>true),
+//            array('name, type, tmp_name, error, size', 'safe'=>true, 'on'=>'upload')
         );
     }
 }
