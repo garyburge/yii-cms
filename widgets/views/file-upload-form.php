7f12658d2d002afb1e80c1db2491d8d8bbdad5fb
Gary Burge <garyburge@garyburge.com>
2013-08-01 07:19:33 -0400

    Fully qualify file-upload-form action attribute with 'cms' module name

--- widgets/views/file-upload-form.php
+++ widgets/views/file-upload-form.php
@@ -1,3 +1,3 @@
-<form id="file-upload-form" class="dropzone" action="<?php echo $this->controller->createUrl('/media/upload'); ?>" method="post">
+<form id="file-upload-form" class="dropzone" action="<?php echo $this->controller->createUrl('/cms/media/upload'); ?>" method="post">
 </form>
 
