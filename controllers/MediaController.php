117d3ee767c04f99f69ba1fae73fd835b699bcd7
Gary Burge <garyburge@garyburge.com>
2013-08-01 16:04:45 -0400

    Still more playing with CUploadedFile

--- controllers/MediaController.php
+++ controllers/MediaController.php
@@ -52,7 +52,8 @@ class MediaController extends Controller
             // create upload form
             $upload = new UploadForm('upload');;
             // copy to model attributes
-            $upload->attributes = $_FILES['file'];
+//            $upload->attributes = $_FILES['file'];
+            $upload->attributes = $_FILES[];
             $aResult['attributes'] = print_r($upload->attributes, true);
 
 //            // copy to model
