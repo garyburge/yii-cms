2176e2ffba6316bcea2c3dd068cf3ecbeeec2432
Gary Burge <garyburge@garyburge.com>
2013-08-01 08:39:36 -0400

    That didn't work. Removed script in _form and removed 'dropzone' class from 'file-upload-form'.

--- widgets/views/file-upload-form.php
+++ widgets/views/file-upload-form.php
@@ -1,4 +1,4 @@
-<form id="file-upload-form" class="dropzone" action="<?php echo $this->controller->createUrl('/cms/media/upload'); ?>" method="post">
+<form id="file-upload-form" action="<?php echo $this->controller->createUrl('/cms/media/upload'); ?>" method="post">
     <div class="fallback">
         <input name="file" type="file" multiple />
     </div>
