a8369e834be5c32b5891e958de7ffdd2e9e76c2e
Gary Burge <garyburge@garyburge.com>
2013-08-01 17:21:33 -0400

    Continue to debug CUploadedFile

--- widgets/views/file-upload-form.php
+++ widgets/views/file-upload-form.php
@@ -1,4 +1,4 @@
-<form id="file-upload-form" action="<?php echo $this->controller->createUrl('/cms/media/upload'); ?>" method="post">
+<form id="file-upload-form" action="<?php echo $this->controller->createUrl('/cms/media/upload'); ?>" method="post" enctype="multipart/form-data">
     <div class="fallback">
         <input name="file" type="file" multiple />
     </div>
