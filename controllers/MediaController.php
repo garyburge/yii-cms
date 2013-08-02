e4a45a4c0c1b023ab5a72ce5dd118c84887f0131
Gary Burge <garyburge@garyburge.com>
2013-08-02 09:14:10 -0400

    User column2 layout

--- controllers/MediaController.php
+++ controllers/MediaController.php
@@ -2,6 +2,11 @@
 
 class MediaController extends Controller
 {
+	/**
+	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
+	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+	 */
+	public $layout='//layouts/column2';
 
     /**
      * @return array action filters
--- views/media/index.php
+++ views/media/index.php
@@ -9,9 +9,15 @@ $this->menu=array(
 );
 ?>
 
-<h1>Medias</h1>
+<h1>Media Library</h1>
 
+<div class="row">
+    <div class="span9">
 <?php $this->widget('bootstrap.widgets.TbListView',array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
+    </div><!-- .span9 -->
+    <div class="span3">
+    </div><!-- .span3 -->
+</div>
