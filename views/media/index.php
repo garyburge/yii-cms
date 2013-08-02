d2a582c41b2fb89e8b1e3a14169502d381b352a0
Gary Burge <garyburge@garyburge.com>
2013-08-02 09:14:39 -0400

    User column2 layout

--- views/media/index.php
+++ views/media/index.php
@@ -11,13 +11,7 @@ $this->menu=array(
 
 <h1>Media Library</h1>
 
-<div class="row">
-    <div class="span9">
 <?php $this->widget('bootstrap.widgets.TbListView',array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
-    </div><!-- .span9 -->
-    <div class="span3">
-    </div><!-- .span3 -->
-</div>
