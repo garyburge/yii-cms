a7da0cf548dc97a1ab37a6bdb2a29e4365995f1f
Gary Burge <garyburge@garyburge.com>
2013-08-02 05:27:50 -0400

    Restored UploadForm as CActiveForm

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -4,14 +4,19 @@
  * UploadForm class.
  *
  */
-class UploadForm extends CActiveRecord
+class UploadForm extends CFormModel
 {
-    public $image = 'file';
+    public $name;
+    public $type;
+    public $tmp_name;
+    public $error;
+    public $size;
+    public $media_id;
 
     public function rules()
     {
         return array(
-            array('image', 'file', 'types'=>'jpg, png, gif'),
+            array('name, type, tmp_name, error, size', 'safe', 'on'=>'upload')
         );
     }
 }
