270a0c9eaa02cc7dff9fe14e5d7a8845e4b8f749
Gary Burge <garyburge@garyburge.com>
2013-08-02 09:52:48 -0400

    use dropDownListRow for media_type_id input

--- views/media/_form.php
+++ views/media/_form.php
@@ -1,32 +1,26 @@
+<?php
+    $sql = "SELECT id, CONCAT(extension, ' (', name, ')') AS value FROM media_type ORDER BY extension"
+    $aTypes = Yii::app()->createCommand($sql)->queryAll(true);
+?>
+
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'media-form',
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="help-block">Fields with <span class="required">*</span> are required.</p>
-
+	<p class="help-block muted"><small>Fields with <span class="required">*</span> are required.</small></p>
 	<?php echo $form->errorSummary($model); ?>
 
-	<?php echo $form->textFieldRow($model,'media_type_id',array('class'=>'span5')); ?>
-
+	<?php echo $form->dropDownListRow($model, 'media_type_id', CHtml::listData($aTypes, 'id', 'value'), array('class'=>'span5')); ?>
 	<?php echo $form->textFieldRow($model,'original_file',array('class'=>'span5','maxlength'=>255)); ?>
-
 	<?php echo $form->textFieldRow($model,'file',array('class'=>'span5','maxlength'=>255)); ?>
-
 	<?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
-
 	<?php echo $form->textAreaRow($model,'caption',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
-
 	<?php echo $form->textFieldRow($model,'attribution',array('class'=>'span5','maxlength'=>255)); ?>
-
 	<?php echo $form->textFieldRow($model,'copyright',array('class'=>'span5','maxlength'=>255)); ?>
-
 	<?php echo $form->textFieldRow($model,'height',array('class'=>'span5')); ?>
-
 	<?php echo $form->textFieldRow($model,'width',array('class'=>'span5')); ?>
-
 	<?php echo $form->textFieldRow($model,'created',array('class'=>'span5')); ?>
-
 	<?php echo $form->textFieldRow($model,'updated',array('class'=>'span5')); ?>
 
 	<div class="form-actions">
