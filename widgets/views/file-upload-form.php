a8b8bdf9bf8c5647a6a75792d3d1b745839de441
Gary Burge <garyburge@garyburge.com>
2013-08-02 14:06:10 -0400

    added hidden 'image' field

--- widgets/views/file-upload-form.php
+++ widgets/views/file-upload-form.php
@@ -1,4 +1,5 @@
 <form id="file-upload-form" action="<?php echo $this->controller->createUrl('/cms/media/imageupload'); ?>" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="image" value="image'>
     <div class="fallback">
         <input name="file" type="file" multiple />
     </div>
