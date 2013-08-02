ba5dcdc0c53bd4e05f2be7391d6a5b5b0a6f103a
Gary Burge <garyburge@garyburge.com>
2013-08-02 11:06:28 -0400

    set url on Dropzone options; add class 'dropzonep only if new record

--- assets/media-form.js
+++ assets/media-form.js
@@ -7,21 +7,22 @@ $(document).ready(function() {
         imageThumbsDir: g_imageThumbsDir
     }
 
+    // set some dropzone options
+    Dropzone.options.fileUploadForm = {
+        url: '/media/upload.html',
+        paramName: 'file',
+        maxFilesize: 2,
+        acceptedFiles: '.jpg, .png, .gif',
+    };
+
     // set dropzone div, image div
     if (app.isNewRecord) {
+        $('#div-no-image').addClass('dropzone');
         $('#div-no-image').show();
         $('#div-with-image').hide();
     } else {
         $('#div-no-image').hide();
         $('#div-with-image').show();
     }
-    
-    // set some dropzone options
-    Dropzone.options.fileUploadForm = {
-        paramName: "file",
-        maxFilesize: 2,
-        acceptedFiles: '.jpg, .png, .gif',
-    };
-
 
 })
