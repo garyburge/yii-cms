03028dc0519b465498187448b35c8e98d43b4e04
Gary Burge <garyburge@garyburge.com>
2013-08-03 18:54:10 -0400

    and yet another.

--- views/media/_view.php
+++ views/media/_view.php
@@ -1,6 +1,6 @@
   <li class="span4">
     <a href="<?php echo $this->module->baseMediaUrl.'/'.$data->file; ?>" class="thumbnail">
-      <img data-src="<?php echo $this->module->baseMediaUrl.'/'.$this->module->mediaThumbDir.'/'.$data->file; ?>" alt="">
+      <img data-src="<?php echo $this->module->baseMediaUrl.'/'.$this->module->imageThumbsDir.'/'.$data->file; ?>" alt="">
     </a>
   </li>
 
