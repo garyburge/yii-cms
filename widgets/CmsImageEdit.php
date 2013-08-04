7039205c650c4870f66c6351a5f4cabe2c0389ab
Gary Burge <garyburge@garyburge.com>
2013-08-04 09:59:57 -0400

    forgot semicolon

--- widgets/CmsImageEdit.php
+++ widgets/CmsImageEdit.php
@@ -28,7 +28,7 @@ class CmsImageEdit extends CWidget
 		$this->easelJsAssetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, false);
 
         // need easeljs script files
-        Yii::app()->clientScript->registerScriptFile($this->easelJsAssetsUrl.'/easeljs-0.6.1.min.js')
+        Yii::app()->clientScript->registerScriptFile($this->easelJsAssetsUrl.'/easeljs-0.6.1.min.js');
     }
 
     public function run()
