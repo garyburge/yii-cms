fe1137fef163e251ff6e822e2d472b6083d47294
Gary Burge <garyburge@garyburge.com>
2013-08-04 13:19:18 -0400

    Added composer.json, package.json

--- widgets/CmsImageEdit.php
+++ widgets/CmsImageEdit.php
@@ -23,12 +23,20 @@ class CmsImageEdit extends CWidget
         Yii::app()->clientScript->registerCoreScript('jquery');
         Yii::app()->clientScript->registerCoreScript('jquery.ui');
 
-        // if not published
+        // publish easelJs
   		$assetsPath = Yii::getPathOfAlias('easeljs.lib');
 		$this->easelJsAssetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, false);
 
-        // need easeljs script files
+        // register easeljs script files
         Yii::app()->clientScript->registerScriptFile($this->easelJsAssetsUrl.'/easeljs-0.6.1.min.js');
+
+        // publish jcrop
+  		$assetsPath = Yii::getPathOfAlias('jcrop');
+		$this->jcropAssetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, false);
+
+        // register Jcrop script, css files
+        Yii::app()->clientScript->registerScriptFile($this->jcropAssetsUrl.'/js/jquery.Jcrop.min.js');
+        Yii::app()->clientScript->registerCssFile($this->jcropAssetsUrl.'/css/jquery.Jcrop.min.css');
 
         // and then our own javascript
         Yii::app()->clientScript->registerScriptFile($this->controller->module->assetsUrl.'/image-edit.js');
